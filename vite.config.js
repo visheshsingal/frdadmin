53732df9570679a1eb5ade5c59a0515be61c329f
visheshsingal <vishesh.singal.contact@gmail.com>
2025-10-01 21:07:37 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port : 5174}
-})
+  define: {
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:4000')
+  },
+  server: {
+    port: 5174 // Local dev port
+  }
+});
+
